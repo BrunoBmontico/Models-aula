afc798a3013779f8e6e07b1a73e8156d2bfcc068
BrunoBmontico <aluno103.23187204bruno.com>
2024-04-11 21:40:08 -0300

    exercicio terminado

--- core/medico/models.py
+++ core/medico/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Medico(models.Model):
+    name = models.CharField(max_length=200)
+    age = models.IntegerField(max_length=2)
+    phone = models.CharField(max_length=20)
